d6ca181d6e4f58619908b6e700ad3d6cb8cc3919
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-11-23 13:53:11 +0200

    feat(examples): ptrace1 - single invocation

---
+++ examples/ptrace1.rs
@@ -0,0 +1,9 @@
+use libc::{ptrace, PTRACE_TRACEME};
+
+fn main() {
+    if unsafe { ptrace(PTRACE_TRACEME, 0, 1, 0) } == -1 {
+        println!("Debugger");
+    } else {
+        println!("Normal");
+    }
+}
