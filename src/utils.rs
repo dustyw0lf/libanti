7b7266e16b37d585fe148c4e2e02bd85586a498b
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-11-30 00:33:56 +0200

    feat(utils): move dynamic library loading into the crate

---
+++ src/utils.rs
@@ -0,0 +1,16 @@
+use std::sync::Once;
+
+use libloading::Library;
+
+static INIT: Once = Once::new();
+static mut LIBC: Option<Library> = None;
+
+pub fn get_lib(lib: &str) -> Result<&'static Library, Box<dyn std::error::Error>> {
+    unsafe {
+        INIT.call_once(|| {
+            LIBC = Some(Library::new(lib).unwrap());
+        });
+
+        LIBC.as_ref().ok_or_else(|| "Failed to load library".into())
+    }
+}
