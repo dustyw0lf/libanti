b113d72c8cf30aff46b26c77fef4f3948a46f6fc
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-11-30 00:56:01 +0200

    refactor(examples): update examples to use the new library API

--- examples/04-ptrace-dynamic.rs
+++ examples/04-ptrace-dynamic.rs
@@ -1,30 +1,13 @@
-use std::ffi::{c_int, c_long, c_uint, c_void};
+use std::ffi::{c_uint, c_void};
 
-use libloading::Symbol;
-
-use linux_antidbg::utils::get_lib;
+use linux_antidbg::detect::ptrace::get_ptrace;
 
 fn main() {
-    let not_ptrace = get_not_ptrace().unwrap();
+    let ptrace = get_ptrace().unwrap();
 
-    if unsafe { not_ptrace(0 as *const c_uint, 0, 0 as *mut c_void, 0 as *mut c_void) } == -1 {
+    if unsafe { ptrace(0 as *const c_uint, 0, 0 as *mut c_void, 0 as *mut c_void) } == -1 {
         println!("Debugger");
     } else {
         println!("Normal");
-    }
-}
-
-type NotPtraceFn = unsafe extern "C" fn(
-    request: *const c_uint,
-    pid: c_int,
-    addr: *mut c_void,
-    data: *mut c_void,
-) -> c_long;
-
-pub fn get_not_ptrace() -> Result<NotPtraceFn, Box<dyn std::error::Error>> {
-    let lib = get_lib()?;
-    unsafe {
-        let not_ptrace: Symbol<NotPtraceFn> = lib.get(b"ptrace\0")?;
-        Ok(*not_ptrace.into_raw())
     }
 }
--- examples/05-ptrace-syscall.rs
+++ examples/05-ptrace-syscall.rs
@@ -1,28 +1,9 @@
-use core::arch::asm;
+use linux_antidbg::detect::ptrace::syscall_ptrace;
 
 fn main() {
-    if unsafe { not_ptrace(0, 0, 0, 0) } == -1 {
+    if unsafe { syscall_ptrace(0, 0, 0, 0) } == -1 {
         println!("Debugger");
     } else {
         println!("Normal");
     }
-}
-
-#[inline]
-unsafe fn not_ptrace(request: usize, pid: usize, addr: usize, data: usize) -> isize {
-    // Based on
-    // https://github.com/jasonwhite/syscalls/blob/main/src/syscall/x86_64.rs
-    let mut ret: usize;
-    asm!(
-        "syscall",
-        inlateout("rax") 101 as usize => ret, // ptrace syscall number
-        in("rdi") request,                    // arg 1
-        in("rsi") pid,                        // arg 2
-        in("rdx") addr,                       // arg 3
-        in("r10") data,                       // arg 4
-        out("rcx") _,                         // rcx is used to store old rip
-        out("r11") _,                         // r11 is used to store old rflags
-        options(nostack, preserves_flags)
-    );
-    ret as isize
 }
