aee188222907a7487660aa6e219a552bc9abad12
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-11-29 23:52:04 +0200

    feat(virt): implement detection based on CPU brand

--- src/virt.rs
+++ src/virt.rs
@@ -1,1 +1,15 @@
+use raw_cpuid::CpuId;
 
+pub fn is_virt_cpu() -> Result<bool, Box<dyn std::error::Error>> {
+    let cpuid = CpuId::new();
+    let brand = cpuid
+        .get_processor_brand_string()
+        .ok_or_else(|| "Failed to get processor brand ID")?;
+    let brand = brand.as_str();
+
+    if brand.contains("Intel") || brand.contains("AMD") {
+        Ok(false)
+    } else {
+        Ok(true)
+    }
+}
