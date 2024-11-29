29e29beb81d763898c38c39ae9a58ab09a17d950
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-11-30 00:58:48 +0200

    docs(examples): add example for virtualization detection

---
+++ examples/detect-virt.rs
@@ -0,0 +1,15 @@
+use linux_antidbg::detect::virt::{is_virt_cpu, is_virt_disk};
+
+fn main() {
+    if is_virt_cpu().unwrap() {
+        println!("CPU: Virtual machine");
+    } else {
+        println!("CPU: Normal");
+    }
+
+    if is_virt_disk().unwrap() {
+        println!("Disk: Virtual machine");
+    } else {
+        println!("Disk: Normal");
+    }
+}
