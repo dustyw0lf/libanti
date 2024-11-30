e8c75ae8df01a0b894bdec363b44742cbd6a1f7a
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-11-30 20:33:51 +0200

    fix(utils): use the updated Error type

--- src/utils.rs
+++ src/utils.rs
@@ -2,26 +2,27 @@ use std::sync::{Once, OnceLock};
 
 use libloading::Library;
 
-use crate::error::{Error, Result};
+use crate::error::{Error, InitError, Result};
 
 static LIBC_INIT: Once = Once::new();
 static mut LIBC: Option<Library> = None;
 static LIBC_INIT_ERROR: OnceLock<String> = OnceLock::new();
 
 pub(crate) fn get_libc() -> Result<&'static Library> {
+    let lib = "libc.so.6";
     unsafe {
-        LIBC_INIT.call_once(|| match Library::new("libc.so.6") {
+        LIBC_INIT.call_once(|| match Library::new(lib) {
             Ok(lib) => LIBC = Some(lib),
-            Err(e) => {
-                let _ = LIBC_INIT_ERROR.set(e.to_string());
+            Err(err) => {
+                let _ = LIBC_INIT_ERROR.set(err.to_string());
             }
         });
 
         if let Some(err) = LIBC_INIT_ERROR.get() {
-            return Err(Error::Other(err.clone()));
+            return Err(Error::Init(InitError::LibraryInit(err)));
         }
 
         LIBC.as_ref()
-            .ok_or_else(|| Error::Other("failed to initialize libc".to_string()))
+            .ok_or_else(|| Error::Init(InitError::LibraryInit(lib)))
     }
 }
